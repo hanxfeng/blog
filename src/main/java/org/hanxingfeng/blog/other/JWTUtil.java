a110625d3bb3bc34ba096f217ec74477ee944c52
hanxfeng <3556050019@qq.com>
2026-06-21 16:45:08 +0800

    新增了一个 JWTUtil 工具类，用于生成和解析 JWT token

---
+++ src/main/java/org/hanxingfeng/blog/other/JWTUtil.java
@@ -0,0 +1,61 @@
+package org.hanxingfeng.blog.other;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+public class JWTUtil {
+    // 随机生成的 key
+    private final String SECRET = "thisIsMyKeysMaybeDemoNCloud";
+    private final Key key =
+            Keys.hmacShaKeyFor(
+                    SECRET.getBytes(StandardCharsets.UTF_8)
+            );
+
+    /**
+     * 用于生成 token
+     * @return 返回生成的 token
+     */
+    public String generateToken() {
+        JwtBuilder jwtBuilder = Jwts.builder();
+
+        return jwtBuilder
+                // 添加 Payload
+                // TODO:不写死，后续根据业务修改
+                .claim("userName", "hxf") // 私有声明
+                .setSubject("2654826") // 注册声明
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 一小时后过期
+                // 添加 Signature
+                .signWith(key) // 这个方法会自动生成 Header 因此不需要再手动写 Header
+                // 将以上三部分用 . 拼接
+                .compact();
+    }
+
+    /**
+     * 用于对 token 进行解析
+     * @param token：需要解析的 token
+     * @return 解析后的内容
+     */
+    public Claims parseToken(String token) {
+        Jws<Claims> claimsJwt = Jwts.parserBuilder()
+                .setSigningKey(key)          // SECRET_KEY 可为 String/byte[]/Key
+                .build()                            // 构建 JwtParser
+                .parseClaimsJws(token);             // 解析并验证 JWS
+
+        return claimsJwt.getBody();
+    }
+
+    /**
+     * 用于判断 JWT 是否过期
+     * @param token：需要进行判断的 token
+     * @return 返回 true 表示 token 已过期
+     */
+    public boolean isExpired(String token) {
+        return parseToken(token)          // 1. 解析 JWT，获取 Claims 对象
+                .getExpiration()          // 2. 取出过期时间（即 exp 字段）
+                .before(new Date());      // 3. 判断该时间是否在当前时间之前
+    }
+}
--- src/test/java/org/hanxingfeng/blog/BlogApplicationTests.java
+++ src/test/java/org/hanxingfeng/blog/BlogApplicationTests.java
@@ -3,6 +3,9 @@ package org.hanxingfeng.blog;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.hanxingfeng.blog.Entity.NodeCount;
 import org.hanxingfeng.blog.Entity.Writings;
@@ -18,9 +21,12 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.security.Key;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 
+import static io.jsonwebtoken.security.Keys.secretKeyFor;
 import static org.hanxingfeng.blog.Entity.SystemConstants.UPLOAD_DIR;
 
 @Slf4j
@@ -58,5 +64,49 @@ class BlogApplicationTests {
         nodeCount.setTodayTime(LocalDate.now().minusDays(1));
         String jsonString = objectMapper.writeValueAsString(nodeCount);
         redisTemplate.opsForValue().set(LocalDate.now().minusDays(1).toString(), jsonString);
+    }
+    private static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    @Test
+    /**
+     * JWT 加密
+     */
+    void jwt() {
+        System.out.println(SECRET_KEY.toString());
+        JwtBuilder jwtBuilder = Jwts.builder();
+        String toke = jwtBuilder
+                // 添加 Header
+                .setHeaderParam("typ", "JWT")
+                .setHeaderParam("alg", "HS256")
+                // 添加 Payload
+                .claim("userName", "hxf") // 私有声明
+                .setSubject("2654826") // 注册声明
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 一小时后过期
+                // 添加 Signature
+                .signWith(SECRET_KEY)
+                // 将以上三部分用 . 拼接
+                .compact();
+        System.out.println(toke);
+
+        Jws<Claims> claimsJwt = Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)          // SECRET_KEY 可为 String/byte[]/Key
+                .build()                            // 构建 JwtParser
+                .parseClaimsJws(toke);             // 解析并验证 JWS
+
+        Claims claims = claimsJwt.getBody();
+        System.out.println(claims.get("userName"));
+    }
+
+    @Test
+    void parse() {
+        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyTmFtZSI6Imh4ZiIsInN1YiI6IjI2NTQ4MjYiLCJleHAiOjE3ODIwMzEyMDB9.HqNtO2zVPJ74hKL2TbUaYV7-eeJ_X1p6Nq8xBzHKTEo";
+
+        // 使用 parserBuilder() 替代废弃的 parser()
+        Jws<Claims> claimsJwt = Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)          // SECRET_KEY 可为 String/byte[]/Key
+                .build()                            // 构建 JwtParser
+                .parseClaimsJws(token);             // 解析并验证 JWS
+
+        Claims claims = claimsJwt.getBody();
+        System.out.println(claims.get("userName"));
     }
 }
